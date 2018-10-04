446edcb3b4816b80c7b746b8ed8ab4a0cb037cc7
shriya <shri17ya@gmail.com>
2018-10-04 18:00:35 -0300

    database first commit

--- app/src/main/java/com/example/shriya/hack3_wie2018/MainActivity.java
+++ app/src/main/java/com/example/shriya/hack3_wie2018/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.shriya.hack3_wie2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    //    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        try {
+//            DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+//            UserInformation user = new UserInformation("bb", "aa", "a", "a", "a");
+//            rootRef.child("new").setValue(user);
+//            DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
+//            DatabaseReference demoRef = databaseReference.child("demo");
+//                    String value = "aa";
+//                    //push creates a unique id in database
+//                    demoRef.setValue(value);
+//            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    int f = 0;
+//                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+//                        Toast.makeText(MainActivity.this,ds.getKey(),Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//                }
+//
+//            });
+//
+//        }
+//        catch (Exception e)
+//        {
+//            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+//        }
+//    }
     }
 }
---
+++ app/src/main/java/com/example/shriya/hack3_wie2018/UserInformation.java
@@ -0,0 +1,19 @@
+package com.example.shriya.hack3_wie2018;
+
+/**
+ * Created by Shriya on 04-10-2018.
+ */
+
+public class UserInformation
+{
+    public static String name,address,email,phoneno,password;
+    public UserInformation(){};
+
+    public UserInformation(String name, String address, String email, String phoneno, String password) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phoneno = phoneno;
+        this.password = password;
+    }
+}
